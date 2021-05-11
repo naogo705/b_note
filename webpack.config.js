8b6a3ee57353a439741a5c18652f2f79649849ad
naogo705 <66290412+naogo705@users.noreply.github.com>
2021-05-11 17:14:07 +0900

    companyページへのリンク設定修正

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = (env, argv) => {
         template: 'src/ejs/about/index.ejs',
       }),
       new HtmlWebpackPlugin({
-        filename: 'company/company.html',
+        filename: 'company/index.html',
         template: 'src/ejs/company/company.ejs',
       }),
 
